54d867e2005cceb32b64b60c664ef29a3a5e5a10
cesarmr-github <c3s1r@proton.me>
2025-03-13 21:18:21 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,9 +2,9 @@ Config = {}
 Config.Locale = GetConvar('esx:locale', 'en')
 
 Config.Webhooks = {
-    General = "https://discord.com/api/webhooks/1252986436618162320/pZrA2nv-6llp29o18Y-zWhN_GRyAE78kNqh_dIXdXIPRwSckmHl2azkj8VEpLiXPwkob",
+    General = "DISCORD_WEBHOOK",
     Jobs = {
-        police = "https://discord.com/api/webhooks/1252986436618162320/pZrA2nv-6llp29o18Y-zWhN_GRyAE78kNqh_dIXdXIPRwSckmHl2azkj8VEpLiXPwkob",
-        mechanic = "https://discord.com/api/webhooks/1252986590221963274/0ZHzM65Zz-aPzV4IocIhDxsbnsmKBeJ6uJFps5KCViMjfpQUbyIYWNbLXCMabUjJYis3",
+        police = "DISCORD_WEBHOOK",
+        mechanic = "DISCORD_WEBHOOK",
     }
 }
