0035682d37860aea30ef3ceb06150f999e6c1288
cesarmr-github <167689141+cesarmr-github@users.noreply.github.com>
2025-03-17 20:49:13 +0100

    Fixed "Updated Bills"

--- client/main.lua
+++ client/main.lua
@@ -3,11 +3,6 @@ local isUIVisible = false
 
 local function showBillsMenu()
     ESX.TriggerServerCallback('esx_billing:getBills', function(bills)
-        -- if #bills <= 0 then
-        --     ESX.ShowNotification(TranslateCap('no_invoices'))
-        --     return
-        -- end
-
         SendNUIMessage({
             type = 'SHOW_BILLS',
             bills = bills
@@ -24,7 +19,7 @@ RegisterCommand('showbills', function()
     end
 end, false)
 
-RegisterKeyMapping('showbills', TranslateCap('keymap_showbills'), 'keyboard', 'F7')
+-- RegisterKeyMapping('showbills', TranslateCap('keymap_showbills'), 'keyboard', 'F7')
 
 AddEventHandler('esx:onPlayerDeath', function() isDead = true end)
 AddEventHandler('esx:onPlayerSpawn', function() isDead = false end)
@@ -38,6 +33,30 @@ end)
 
 RegisterNUICallback('payBill', function(data, cb)
     ESX.TriggerServerCallback('esx_billing:payBill', function(resp)
+        if resp then
+            showBillsMenu()
+        end
         cb({ success = resp })
     end, data.billId)
+end)
+
+RegisterNUICallback('getBills', function(data, cb)
+    ESX.TriggerServerCallback('esx_billing:getBills', function(bills)
+        cb({ bills = bills })
+    end)
+end)
+
+RegisterNUICallback('getNearby', function(data, cb)
+    local closestPlayer, closestDistance = ESX.Game.GetClosestPlayer()
+
+    if closestPlayer == -1 or closestDistance > 3.0 then
+        ESX.ShowNotification('There\'s no players nearby!')
+    end
+        cb({ targetId = GetPlayerServerId(closestPlayer) })
+end)
+
+RegisterNUICallback('sendBill', function(data, cb)
+    ESX.TriggerServerCallback('esx_billing:getBills', function(bills)
+        cb({ bills = bills })
+    end)
 end)
